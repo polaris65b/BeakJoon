b79f359a61e492142c95595f56eebac95668f863
polaris65b <polaris65b@gmail.com>
2025-04-01 22:57:08 +0900

    sol : P10811

---
+++ src/bronze/Lv2/P10811.java
@@ -0,0 +1,37 @@
+package bronze.Lv2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P10811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int[] result = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            result[i] = i + 1;
+        }
+
+        for (int k = 0; k < M; k++) {
+            String[] inputNUm = br.readLine().split(" ");
+            int i = Integer.parseInt(inputNUm[0]) - 1;
+            int j = Integer.parseInt(inputNUm[1]) - 1;
+
+            for (int l = i; l < j; l++) {
+                int temp = result[l];
+                result[l] = result[j];
+                result[j] = temp;
+                j--;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
