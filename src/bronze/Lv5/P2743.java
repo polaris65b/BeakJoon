77c31d60234e6466639d1acae0e7a239cb61eb7a
polaris65b <polaris65b@gmail.com>
2025-04-10 23:59:00 +0900

    feat: sol P2743

---
+++ src/bronze/Lv5/P2743.java
@@ -0,0 +1,15 @@
+package bronze.Lv5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        System.out.println(word.length());
+    }
+}
