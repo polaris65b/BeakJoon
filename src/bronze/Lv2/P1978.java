8533f7dd73a7cf57caffaf53bbb78fc11ac13324
polaris65b <polaris65b@gmail.com>
2025-07-10 22:49:50 +0900

    sol:P1978

---
+++ src/bronze/Lv2/P1978.java
@@ -0,0 +1,26 @@
+package bronze.Lv2;
+
+import java.util.Scanner;
+
+public class P1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+} 
