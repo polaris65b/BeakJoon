8b2f19ba42b32bf9637bb99fb657f7465554f1d2
polaris65b <polaris65b@gmail.com>
2025-07-12 23:36:29 +0900

    sol:P2581

---
+++ src/bronze/Lv2/P2581.java
@@ -0,0 +1,38 @@
+package bronze.Lv2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+} 
